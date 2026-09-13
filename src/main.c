a5e79ef1183839b7a34b27ce9bb8e4b3b36fde6c
Daniel Akintaju <dakintaju@gmail.com>
2026-09-13 20:59:00 +0000

    feat: add main. Entry point for entire TCP server

---
+++ src/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "server.h"
+
+#define MIN_PORT 1024
+#define MAX_PORT 65535
+
+int main(int argc, char *argv[]) {
+	int base = 10;
+	long port;
+	char *endptr;
+
+	if (argc != 2) {
+		fprintf(stderr, "Invalid: Input Port Number\n");
+		return 1;
+	}
+
+	port = strtol(argv[1], &endptr, base);
+
+	if (*endptr == '\0') {
+		if (MIN_PORT <= port && port <= MAX_PORT) {
+			return run_server((int) port);
+		}
+
+		else {
+			fprintf(stderr, "Port Number %d out of range\n", port);
+			return 1;
+		}
+	}
+
+	else {
+		fprintf(stderr, "Invalid Port Number\n");
+		return 1;
+	}
+}
+
+
